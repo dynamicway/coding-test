d1406f3b19428c7df67333b0c1388ffc327e5661
bee <rkddbtjd123@gmail.com>
2024-03-20 13:15:57 +0900

    feat: Add when there are no element tests on PermutationTest

--- src/test/java/me/study/combinatorics/PermutationTest.java
+++ src/test/java/me/study/combinatorics/PermutationTest.java
@@ -9,8 +9,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PermutationTest {
 
     @Test
+    void when_there_are_no_element() {
+        Permutation<Integer> sut = new Permutation<>(List.of());
+
+        assertThat(sut.permute()).isEmpty();
+    }
+
+    @Test
     void when_there_is_only_one_element() {
         Permutation<Integer> sut = new Permutation<>(List.of(1));
+
         assertThat(sut.permute())
                 .containsExactlyInAnyOrder(List.of(1));
     }
@@ -18,6 +26,7 @@ class PermutationTest {
     @Test
     void when_there_are_two_elements() {
         Permutation<Integer> sut = new Permutation<>(List.of(1, 2));
+
         assertThat(sut.permute())
                 .containsExactlyInAnyOrder(List.of(1, 2), List.of(2, 1));
     }
@@ -25,6 +34,7 @@ class PermutationTest {
     @Test
     void when_there_are_three_elements() {
         Permutation<Integer> sut = new Permutation<>(List.of(1, 2, 3));
+
         assertThat(sut.permute())
                 .containsExactlyInAnyOrder(List.of(1, 2, 3),
                                            List.of(1, 3, 2),
