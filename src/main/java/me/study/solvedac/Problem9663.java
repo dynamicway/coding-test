b25f463a1b980bf0a39b9bc50f7e5812b6122111
bee <rkddbtjd123@gmail.com>
2024-03-28 10:46:40 +0900

    feat: Add Problem9663.Process1

--- src/main/java/me/study/solvedac/Problem9663.java
+++ src/main/java/me/study/solvedac/Problem9663.java
@@ -6,60 +6,72 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Problem9663 implements CodingTest {
-    static Set<Set<Coordinate>> results = new HashSet<>();
-
     static int solve(int n) {
-        Set<Coordinate> board = createBoard(n);
-
-        for (final Coordinate coordinate : board) {
-            Set<Coordinate> remaining = new HashSet<>(board);
-            remaining.remove(coordinate);
-            Set<Coordinate> current = new HashSet<>();
-            current.add(coordinate);
-            recursive(n, remaining, coordinate, current);
-        }
-
-        return results.size();
+        return new Process1(n).solve();
     }
 
-    private static void recursive(int n, Set<Coordinate> remaining, Coordinate cursor, Set<Coordinate> current) {
-        if (current.size() == n) {
-            results.add(current);
-        } else {
-            removeNotAvailableCoordinates(n, cursor, remaining);
+    private static class Process1 {
+        private final Set<Set<Coordinate>> results = new HashSet<>();
+        private final int n;
 
-            for (final Coordinate coordinate : remaining) {
-                Set<Coordinate> nextRemaining = new HashSet<>(remaining);
-                nextRemaining.remove(coordinate);
-                Set<Coordinate> nextCurrent = new HashSet<>(current);
-                nextCurrent.add(coordinate);
-                recursive(n, nextRemaining, coordinate, nextCurrent);
+        private Process1(int n) {
+            this.n = n;
+        }
+
+        int solve() {
+            Set<Coordinate> board = createBoard();
+
+            for (final Coordinate coordinate : board) {
+                Set<Coordinate> remaining = new HashSet<>(board);
+                remaining.remove(coordinate);
+                Set<Coordinate> current = new HashSet<>();
+                current.add(coordinate);
+                recursive(remaining, coordinate, current);
+            }
+
+            return results.size();
+        }
+
+        private void recursive(Set<Coordinate> remaining, Coordinate cursor, Set<Coordinate> current) {
+            if (current.size() == n) {
+                results.add(current);
+            } else {
+                removeNotAvailableCoordinates(n, cursor, remaining);
+
+                for (final Coordinate coordinate : remaining) {
+                    Set<Coordinate> nextRemaining = new HashSet<>(remaining);
+                    nextRemaining.remove(coordinate);
+                    Set<Coordinate> nextCurrent = new HashSet<>(current);
+                    nextCurrent.add(coordinate);
+                    recursive(nextRemaining, coordinate, nextCurrent);
+                }
             }
         }
-    }
 
-    private static void removeNotAvailableCoordinates(int n, Coordinate cursor, Set<Coordinate> nextRemaining) {
-        for (int i = 0; i < n; i++) {
-            nextRemaining.remove(new Coordinate(cursor.x, i));
-            nextRemaining.remove(new Coordinate(i, cursor.y));
-            nextRemaining.remove(new Coordinate(cursor.x - i, cursor.y - i));
-            nextRemaining.remove(new Coordinate(cursor.x - i, cursor.y + i));
-            nextRemaining.remove(new Coordinate(cursor.x + i, cursor.y - i));
-            nextRemaining.remove(new Coordinate(cursor.x + i, cursor.y + i));
-        }
-    }
-
-    private static Set<Coordinate> createBoard(int n) {
-        Set<Coordinate> board = new HashSet<>();
-        for (int x = 0; x < n; x++) {
-            for (int y = 0; y < n; y++) {
-                board.add(new Coordinate(x, y));
+        private static void removeNotAvailableCoordinates(int n, Coordinate cursor, Set<Coordinate> nextRemaining) {
+            for (int i = 0; i < n; i++) {
+                nextRemaining.remove(new Coordinate(cursor.x, i));
+                nextRemaining.remove(new Coordinate(i, cursor.y));
+                nextRemaining.remove(new Coordinate(cursor.x - i, cursor.y - i));
+                nextRemaining.remove(new Coordinate(cursor.x - i, cursor.y + i));
+                nextRemaining.remove(new Coordinate(cursor.x + i, cursor.y - i));
+                nextRemaining.remove(new Coordinate(cursor.x + i, cursor.y + i));
             }
         }
-        return board;
+
+        private Set<Coordinate> createBoard() {
+            Set<Coordinate> board = new HashSet<>();
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < n; y++) {
+                    board.add(new Coordinate(x, y));
+                }
+            }
+            return board;
+        }
+
     }
 
-    static class Coordinate {
+    private static class Coordinate {
         private final int x;
         private final int y;
 
